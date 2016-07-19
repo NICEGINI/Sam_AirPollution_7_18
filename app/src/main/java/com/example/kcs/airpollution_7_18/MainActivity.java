406c0e96c5cbc57dd5fd32e2939a4e47f6276d93
NICEGINI <cndtjq22@gmail.com>
2016-07-19 15:19:20 -0700

    sec

--- app/src/main/java/com/example/kcs/airpollution_7_18/MainActivity.java
+++ app/src/main/java/com/example/kcs/airpollution_7_18/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //dddd
+        AirSetUp();
         findViewById(R.id.btn_sensor).setOnClickListener(mClickListener);
         AirSetUp();
     }
